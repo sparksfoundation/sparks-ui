d9b88e012f66b88ebb481605fa5f0de452357ab9
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2023-06-05 11:57:45 -0400

    finish setup

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,14 @@ export * from './components/Inputs'
 export * from './components/NoiseBackground'
 export * from './components/Text'
 export * from './components/Triangle'
+
+/**
+ * todo: 
+ * - select box
+ * - checkbox
+ * - multi select
+ * - radio
+ * - textarea
+ * - dropdown
+ * - tooltip
+ */
